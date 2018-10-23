6a2d58fad1fad6d9e3abab942c9a26255b9a51d2
a0939732111@gmail.com <a0939732111@gmail.com>
2018-10-23 16:01:06 +0800

    update dialog style

--- src/components/AlertDialog.vue
+++ src/components/AlertDialog.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
-  .modal(role="dialog" v-if="show" style="display: block;")
-    .modal-dialog(role="document")
-      .modal-content
-        .modal-header
-          h5.modal-title {{ title }}
-          button.close(type="button" data-dismiss="modal" aria-label="Close")
-            span(aria-hidden="true") &times;
-        .modal-body
-          p {{ body }}
-        .modal-footer
-          button.btn.btn-secondary(type="button" data-dismiss="modal" @click="onClicked(false)") 取消
-          button.btn.btn-primary(type="button" @click="onClicked(true)") 確定
+  transition-group(name="fade" tag="div")
+    .modal(role="dialog" v-if="show" style="display: block; transform: translateY(5rem)" key="modal")
+      .modal-dialog(role="document")
+        .modal-content
+          .modal-header
+            h5.modal-title {{ title }}
+            button.close(type="button" data-dismiss="modal" aria-label="Close")
+              span(aria-hidden="true") &times;
+          .modal-body
+            p {{ body }}
+          .modal-footer
+            button.btn.btn-secondary(type="button" data-dismiss="modal" @click="onClicked(false)") 取消
+            button.btn.btn-primary(type="button" @click="onClicked(true)") 確定
 </template>
 
 <script>
@@ -33,6 +34,11 @@ export default {
 }
 </script>
 
-<style lang='sass'>
-
+<style lang='scss'>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
