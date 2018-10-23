72a62796bdc4b065be5f518bdf96d1c458c1cfc3
a0939732111@gmail.com <a0939732111@gmail.com>
2018-10-23 15:18:37 +0800

    update

--- src/components/MyQRcode.vue
+++ src/components/MyQRcode.vue
@@ -5,7 +5,8 @@
     br
     br
     qrcode(:value="qrcode" :options="options" tag="img")
-    QrcodeReader
+    p {{ value }}
+    QrcodeReader(@init="onInit" @decode="onDecode" :paused="paused")
 </template>
 
 <script>
@@ -19,7 +20,7 @@ export default {
   },
   data () {
     return {
-      qrcode: '',
+      qrcode: 'https://piedasing.github.io/qrcode/dist/',
       options: {
         background: 'white',
         backgroundAlpha: '1.0',
@@ -28,15 +29,47 @@ export default {
         mime: 'image/png',
         padding: '0',
         size: 150,
-        level: 'L'
+        level: 'L',
+        // ------------------------------------------------
+        value: '',
+        paused: false
       }
     }
   },
   methods: {
+    onInit (promise) {
+      promise.then(() => {
+        console.log('Successfully initilized! Ready for scanning now!')
+      }).catch(error => {
+        if (error.name === 'NotAllowedError') {
+          this.errorMessage = 'Hey! I need access to your camera'
+        } else if (error.name === 'NotFoundError') {
+          this.errorMessage = 'Do you even have a camera on your device?'
+        } else if (error.name === 'NotSupportedError') {
+          this.errorMessage = 'Seems like this page is served in non-secure context (HTTPS, localhost or file://)'
+        } else if (error.name === 'NotReadableError') {
+          this.errorMessage = 'Couldn\'t access your camera. Is it already in use?'
+        } else if (error.name === 'OverconstrainedError') {
+          this.errorMessage = 'Constraints don\'t match any installed camera. Did you asked for the front camera although there is none?'
+        } else {
+          this.errorMessage = 'UNKNOWN ERROR: ' + error.message
+        }
+      })
+    },
+    onDecode (value) {
+      this.value = value
+      this.paused = true
+    }
   }
 }
 </script>
 
 <style lang='sass'>
-
+input
+  width: 300px
+  height: 30px
+  text-align: center
+  font-size: 1rem
+  border: none
+  border-bottom: 1px solid #666
 </style>
