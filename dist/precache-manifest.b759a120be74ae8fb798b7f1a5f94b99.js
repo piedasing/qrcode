9d554b7a685a5dc07e0e595688074b4633193939
a0939732111@gmail.com <a0939732111@gmail.com>
2018-10-23 17:14:47 +0800

    add pwa

---
+++ dist/precache-manifest.b759a120be74ae8fb798b7f1a5f94b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "44e526246f762d1105d2",
+    "url": "js/chunk-vendors.d623c439.js"
+  },
+  {
+    "revision": "4dfcfe8e2fa4c57f1170",
+    "url": "js/app.f02a889c.js"
+  },
+  {
+    "revision": "da9fde9588b8d0e62538e4c1acc65b8e",
+    "url": "index.html"
+  },
+  {
+    "revision": "44e526246f762d1105d2",
+    "url": "css/chunk-vendors.f21e5dd6.css"
+  },
+  {
+    "revision": "4dfcfe8e2fa4c57f1170",
+    "url": "css/app.2d84040e.css"
+  }
+];
--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-// import VueQrcodeReader from 'vue-qrcode-reader'
 import VueQrcode from '@xkeshi/vue-qrcode'
+import './registerServiceWorker'
 
-// Vue.use(VueQrcodeReader)
 Vue.component(VueQrcode.name, VueQrcode)
 
 Vue.config.productionTip = false
